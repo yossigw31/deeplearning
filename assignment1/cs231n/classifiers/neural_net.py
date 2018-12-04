89e5874fa129ce427b3c5e55cecc2f03ff7bd5a0
Yotam321 <yotam321@gmail.com>
2018-12-04 22:35:08 +0200

    added train & predict

--- assignment1/cs231n/classifiers/neural_net.py
+++ assignment1/cs231n/classifiers/neural_net.py
@@ -177,46 +177,48 @@ class TwoLayerNet(object):
     val_acc_history = []
 
     for it in xrange(num_iters):
-      X_batch = None
-      y_batch = None
+        #########################################################################
+        # TODO: Create a random minibatch of training data and labels, storing  #
+        # them in X_batch and y_batch respectively.                             #
+        #########################################################################
+        X_indices = np.random.choice(num_train, batch_size, replace=False)
+        X_batch = np.take(X, X_indices, axis=0)
+        y_batch = np.take(y, X_indices, axis=0)
+        #########################################################################
+        #                             END OF YOUR CODE                          #
+        #########################################################################
 
-      #########################################################################
-      # TODO: Create a random minibatch of training data and labels, storing  #
-      # them in X_batch and y_batch respectively.                             #
-      #########################################################################
-      pass
-      #########################################################################
-      #                             END OF YOUR CODE                          #
-      #########################################################################
+        # Compute loss and gradients using the current minibatch
+        loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
+        loss_history.append(loss)
 
-      # Compute loss and gradients using the current minibatch
-      loss, grads = self.loss(X_batch, y=y_batch, reg=reg)
-      loss_history.append(loss)
+        #########################################################################
+        # TODO: Use the gradients in the grads dictionary to update the         #
+        # parameters of the network (stored in the dictionary self.params)      #
+        # using stochastic gradient descent. You'll need to use the gradients   #
+        # stored in the grads dictionary defined above.                         #
+        #########################################################################
+        self.params['W1'] -= learning_rate * grads['W1']        
+        self.params['b1'] -= learning_rate * self.params['b1']
+        self.params['W2'] -= learning_rate * grads['W2']        
+        self.params['b2'] -= learning_rate * self.params['b2']
+        #########################################################################
+        #                             END OF YOUR CODE                          #
+        #########################################################################
 
-      #########################################################################
-      # TODO: Use the gradients in the grads dictionary to update the         #
-      # parameters of the network (stored in the dictionary self.params)      #
-      # using stochastic gradient descent. You'll need to use the gradients   #
-      # stored in the grads dictionary defined above.                         #
-      #########################################################################
-      pass
-      #########################################################################
-      #                             END OF YOUR CODE                          #
-      #########################################################################
-
-      if verbose and it % 100 == 0:
-        print('iteration %d / %d: loss %f' % (it, num_iters, loss))
+        if verbose and it % 100 == 0:
+            print('iteration %d / %d: loss %f' % (it, num_iters, loss))
 
       # Every epoch, check train and val accuracy and decay learning rate.
-      if it % iterations_per_epoch == 0:
-        # Check accuracy
-        train_acc = (self.predict(X_batch) == y_batch).mean()
-        val_acc = (self.predict(X_val) == y_val).mean()
-        train_acc_history.append(train_acc)
-        val_acc_history.append(val_acc)
+        if it % iterations_per_epoch == 0:
+            # Check accuracy
+            train_acc = (self.predict(X_batch) == y_batch).mean()
+            val_acc = (self.predict(X_val) == y_val).mean()
+            train_acc_history.append(train_acc)
+            val_acc_history.append(val_acc)
 
-        # Decay learning rate
-        learning_rate *= learning_rate_decay
+            # Decay learning rate
+            learning_rate *= learning_rate_decay
 
     return {
       'loss_history': loss_history,
@@ -244,7 +246,14 @@ class TwoLayerNet(object):
     ###########################################################################
     # TODO: Implement this function; it should be VERY simple!                #
     ###########################################################################
-    pass
+    W1 = self.params['W1']
+    b1 = self.params['b1']
+    W2 = self.params['W2']
+    b2 = self.params['b2']
+    XW1 = X.dot(W1) + b1
+    afterRelu = np.maximum(0, XW1)
+    scores = afterRelu.dot(W2) + b2
+    y_pred = np.argmax(scores, axis=1)
     ###########################################################################
     #                              END OF YOUR CODE                           #
     ###########################################################################
