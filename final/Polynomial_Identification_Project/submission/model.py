67abb9552f1c9f4a8136742250dfc0ec38813f12
yossigw31 <yossigw31@gmail.com>
2019-03-15 11:21:53 +0200

    working!

--- final/Polynomial_Identification_Project/submission/model.py
+++ final/Polynomial_Identification_Project/submission/model.py
@@ -2,71 +2,49 @@ class PolyNet(nn.Module):    # nn.Module is parent class
     def __init__(self):
         super(PolyNet, self).__init__()  # calls init of parent class
         self.layer1 = nn.Sequential(nn.Conv2d(in_channels=3,
-                                              out_channels=10,
+                                              out_channels=3,
                                               kernel_size=3,
                                               stride=1),
-                                              nn.ReLU(),
-                                              nn.BatchNorm2d(10))
-                                                # 126
-
-        self.layer2 = nn.Sequential(nn.Conv2d(in_channels=10,
+                                     nn.ReLU(),
+                                     nn.BatchNorm2d(3))
+#         126
+        self.layer2 = nn.Sequential(nn.Conv2d(in_channels=3,
                                               out_channels=9,
                                               kernel_size=3,
                                               stride=1),
                                               nn.ReLU(),
-                                              # nn.MaxPool2d(2),
+                                              nn.MaxPool2d(2),
                                               nn.BatchNorm2d(9))
-                                                # 124
-            
+#      62   
         self.layer3 = nn.Sequential(nn.Conv2d(in_channels=9,
-                                              out_channels=8,
-                                              kernel_size=2,
+                                              out_channels=9,
+                                              kernel_size=1,
                                               stride=1),
                                               nn.ReLU(),
-                                              # nn.MaxPool2d(2),
-                                              nn.BatchNorm2d(8))
-                                                # 123
-            
-        self.layer4 = nn.Sequential(nn.Conv2d(in_channels=8,
-                                              out_channels=8,
-                                              kernel_size=2),
-                                              nn.ReLU(),
                                               nn.MaxPool2d(2),
-                                              nn.BatchNorm2d(8))
-                                                # 61
+                                              nn.BatchNorm2d(9))
+    #30
+        self.layer4 = nn.Sequential(nn.Conv2d(in_channels=9,
+                                                  out_channels=4,
+                                                  kernel_size=(1)),
+                                                  nn.ReLU(),
+                                                  nn.MaxPool2d(2),
+                                                  nn.BatchNorm2d(4))
         
-        self.layer5 = nn.Sequential(nn.Conv2d(in_channels=8,
-                                              out_channels=12,
-                                              kernel_size=2),
-                                              nn.ReLU(),
-                                              nn.MaxPool2d(2),
-                                              nn.BatchNorm2d(12))
-                                                # 30        
-    
-        self.layer6 = nn.Sequential(nn.Conv2d(in_channels=12,
-                                              out_channels=5,
-                                              kernel_size=3),
-                                              nn.LeakyReLU(),
-                                              # nn.MaxPool2d(3),
-                                              nn.BatchNorm2d(5))
-                                                # 28        
-        
-        self.layer7 = nn.Sequential(nn.Conv2d(in_channels=5,
-                                              out_channels=8,
-                                              kernel_size=1),
-                                              nn.ReLU(),
-                                              nn.MaxPool2d(2),
-                                              nn.BatchNorm2d(8))
-                                                # 14
-        
-        self.layer8 = nn.Sequential(nn.Conv2d(in_channels=8,
-                                              out_channels=3,
-                                              kernel_size=3),
-                                              nn.ReLU(),
-                                              nn.MaxPool2d(2),
-                                              nn.BatchNorm2d(3))
-                                                # 6
+        self.layer5 = nn.Sequential(nn.Conv2d(in_channels=4,
+                                                  out_channels=10,
+                                                  kernel_size=(1)),
+                                                  nn.Tanh(),
+                                                  nn.BatchNorm2d(10))
+    #15
+        self.layer6 = nn.Sequential(nn.Conv2d(in_channels=10,
+                                                  out_channels=3,
+                                                  kernel_size=(4)),
+                                                  nn.LeakyReLU(),
+                                                  nn.MaxPool2d(2),
+                                                  nn.BatchNorm2d(3))
 
+#         self.layer3 = nn.Sequential(nn.MaxPool2d(2,2)
         self.fc1 = nn.Linear(6*6*3, 4)
         # ----------------------------------------------
         # implementation needed here
@@ -79,8 +57,6 @@ class PolyNet(nn.Module):    # nn.Module is parent class
         out = self.layer4(out)
         out = self.layer5(out)
         out = self.layer6(out)
-        out = self.layer7(out)
-        out = self.layer8(out)
         out = out.reshape(out.size(0), -1)
         out = self.fc1(out)
         """
--- final/Polynomial_Identification_Project/submission/optimizer.py
+++
@@ -1,4 +0,0 @@
-polyNet3 = PolyNet().to(device)
-polyNet3.apply(init_weights)
-optimizer3= torch.optim.RMSprop(polyNet3.parameters(), lr=0.01, alpha=0.99, weight_decay=0.05)
-exp_lr_scheduler3 = lr_scheduler.StepLR(optimizer3,step_size=4, gamma=0.8)
